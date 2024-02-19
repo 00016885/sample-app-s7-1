b1d1ded290b3f9cbbe6b6b4a2764974095e6048a
00016885 <wiuts11@gmail.com>
2024-02-19 10:08:36 +0500

    I created for home and add_update

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -1,6 +1,12 @@
 const home_controller = {
     index: async (req, res) => {
         res.render('home');
+    },
+    add: async (req, res) => {
+        res.render('home/add_update');
+    },
+    update: async (req, res) => {
+        res.render('home/add_update');
     }
 };
 
